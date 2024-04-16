119c3ba663c4d0245bec5f28fef4c946d1c7be2c
sahanvimukthi75 <137777547+sahanvimukthi75@users.noreply.github.com>
2024-04-16 19:09:20 +0530

    second commit

--- AT_digital_project/src/components/Navbar.jsx
+++ AT_digital_project/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { AiOutlineMenuUnfold } from "react-icons/ai";
 
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
+
   const handleChange = () => {
     setMenu(!menu);
   };
@@ -12,8 +13,8 @@ const Navbar = () => {
   const closeMenu = () => {
     setMenu(false);
   };
+
   return (
-    
     <div className="fixed w-full bg-navbarcolor">
       <div className="flex flex-row justify-between p-3 md:px-32 px-3 bg-navbarcolor shadow-[0_3px_10px_rgba(0,0,0,0.2)]">
         <div className="flex flex-row items-center cursor-pointer">
@@ -57,55 +58,63 @@ const Navbar = () => {
             Careers
           </Link>
         </nav>
-        <div>
-          <div className="md:hidden flex items-center">
-            {menu ? (
-              <AiOutlineClose size={25} onClick={handleChange} />
-            ) : (
-              <AiOutlineMenuUnfold size={25} onClick={handleChange} />
-            )}
-          </div>
-        </div>
-        <div
-          className={`${
-            menu ? "translate-x-0" : "-translate-x-full"
-          } lg:hidden flex flex-col absolute bg-white text-black left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}
-          style={{ height: '700px' }} 
-        >
-          <Link
-            to="services"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className="hover:text-brightColor transition-all cursor-pointer"
-          >
-            Services
-          </Link>
-
-          <Link
-            to="about"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className="hover:text-brightColor transition-all cursor-pointer"
-          >
-            About Us
-          </Link>
-
-          <Link
-            to="careers"
-            spy={true}
-            smooth={true}
-            duration={500}
-            className="hover:text-brightColor transition-all cursor-pointer"
-          >
-            Careers
-          </Link>
+        <div className="md:hidden flex items-center">
+          {menu ? (
+            <AiOutlineClose size={25} onClick={handleChange} />
+          ) : (
+            <AiOutlineMenuUnfold size={25} onClick={handleChange} />
+          )}
         </div>
       </div>
+      {/* Mobile Menu */}
+      <div
+        className={`${
+          menu ? "translate-x-0" : "-translate-x-full"
+        } lg:hidden flex flex-col absolute bg-white text-black left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full transition-transform duration-300`}
+        style={{ minHeight: menu ? '700px' : '0px', transition: 'min-height 0.3s ease-in-out' }}
+      >
+        <Link
+          to="services"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:text-brightColor transition-all cursor-pointer"
+          onClick={closeMenu}
+        >
+          Services
+        </Link>
+        <Link
+          to="about"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:text-brightColor transition-all cursor-pointer"
+          onClick={closeMenu}
+        >
+          About Us
+        </Link>
+        <Link
+          to="contact"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:text-brightColor transition-all cursor-pointer"
+          onClick={closeMenu}
+        >
+          Contact Us
+        </Link>
+        <Link
+          to="careers"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:text-brightColor transition-all cursor-pointer"
+          onClick={closeMenu}
+        >
+          Careers
+        </Link>
+      </div>
     </div>
-    
-    
   );
 };
 
--- AT_digital_project/src/components/Services.jsx
+++ AT_digital_project/src/components/Services.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React from "react";
 import background from "../assets/laptop.jpg";
-import Button from './layouts/Button';
+import Button from "./layouts/Button";
 
 const Services = () => {
   return (
-    <div className="min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-cover bg-no-repeat" style={{ backgroundImage: `url(${background})` }}>
-      <div className="w-full lg:w-2/3">
-        <div className=" bg-gradient-to-br from-green-400 to-blue-400 p-8 rounded-lg" style={{ marginTop: '300px', marginLeft: '50px', marginRight:'100px'}}>
-          <h1 className="font-semibold text-6xl text-white">
-            We crush your competitors, goals, and sales records - without the B.S.
+    <div
+      className="min-h-screen flex flex-col lg:flex-row justify-center items-center lg:px-32 px-5 bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className="w-full lg:w-2/3 flex justify-center lg:justify-start">
+        <div
+          className="bg-gradient-to-br from-green-400 to-blue-400 p-8 rounded-lg"
+          style={{
+            maxWidth: "600px",
+            marginLeft: "-200px",
+            marginBottom: "-300px",
+          }}
+        >
+          <h1
+            className="font-semibold text-5xl text-white mb-8"
+            style={{ width: "556px", height: "192px" }}
+          >
+            We crush your competitors, goals, and sales records - without the
+            B.S.
           </h1>
+
           <Button title="Get free consultation" />
         </div>
       </div>
